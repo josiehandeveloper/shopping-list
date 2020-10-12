d1621c0f59b1e1f14b2b829a93d4a2fa5d89a8a5
josiehandeveloper <63170710+josiehandeveloper@users.noreply.github.com>
2020-10-11 17:40:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 $(function(){
-    $('#js-shopping-list-form').submit(function(event){
-        event.preventDefault();
-        const listitem = $('.js-shopping-list-entry').val();
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const listItem = $('.js-shopping-list-entry').val();
 
-        $('#shopping-list-entry').val('');
+    $('#shopping-list-entry').val('');
 
-        $('.shopping-list').append(
-           `<li>
-             <span class="shopping-item">${listItem}</span>
-             <div class="shopping-item-controls">
+
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
             <span class="button-label">check</span>
           </button>
@@ -17,12 +18,14 @@ $(function(){
           </button>
         </div>
       </li>`);
-    });
+  });
 
-    $('shopping-list').on('click', '.shopping-item-delete', function(event){
-      $(this).closest('li').remove();
-    });
-    $('shopping-list').on('click', '.shopping-item-toggle', function(event){
-      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');  
-    });
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
 });
